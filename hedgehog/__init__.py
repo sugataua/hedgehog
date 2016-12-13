e9e2de17a7ee055587b8bccec410a0f3be7e9e85
Sugata <sugataua@sugataua.com>
2016-12-14 00:18:26 +0200

    using flask-login

--- hedgehog/__init__.py
+++ hedgehog/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from hedgehog import configmodule
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = '\x9c\xcd\xe4\x1c\xd1b\x01\x1c\xf8\xda\\h\xe1L\x92\xa9@2\xb1'
@@ -13,6 +14,8 @@ db = SQLAlchemy(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 """ Config part """
 app.config.from_object(configmodule.DevelopmentConfig)
